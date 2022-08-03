85255663ab9abb19a04d10c9212352330fca573e
s10220975 <s10220975@connect.np.edu.sg>
2022-08-03 11:01:57 +0800

    hello

--- project_group/read_file.py
+++ project_group/read_file.py
@@ -5,11 +5,10 @@ import csv
 full_list = []
 overhead_list = []
 
-file_path = Path.cwd()/'csv_reports'/'overheads.csv'
+file_path = Path.cwd()/'csv_reports'/'Overheads.csv'
 with file_path.open(mode= 'r', encoding= 'UTF-8') as file:
     reader = csv.reader(file)
     header = next(reader)
     print (header)
     for line in reader:
         full_list.append(line)
-        # ???
