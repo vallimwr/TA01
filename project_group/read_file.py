aa3fab10945bed5779758f97b720287d62c5d3d0
s10220975 <s10220975@connect.np.edu.sg>
2022-08-03 11:05:42 +0800

    hello

--- project_group/read_file.py
+++ project_group/read_file.py
@@ -1,14 +1,3 @@
-from api import forex
 from pathlib import Path
 import csv
 
-full_list = []
-overhead_list = []
-
-file_path = Path.cwd()/'csv_reports'/'Overheads.csv'
-with file_path.open(mode= 'r', encoding= 'UTF-8') as file:
-    reader = csv.reader(file)
-    header = next(reader)
-    print (header)
-    for line in reader:
-        full_list.append(line)
