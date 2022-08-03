68ecab39b00a3b176708838742879c040f813d3b
s10220975 <s10220975@connect.np.edu.sg>
2022-08-03 10:26:06 +0800

    kfjkfsfksef

--- project_group/api.py
+++ project_group/api.py
@@ -4,7 +4,6 @@ api_key = "1JZVGRKCHDT3KUSQ"
 url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
 
 r = requests.get(url)
-
 data = r.json()
 
 exchange_rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
