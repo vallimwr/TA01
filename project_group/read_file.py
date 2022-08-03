d35def012da1ad9954ae6f8cd9973d751aaab3c7
Kaylene <s10243062@connect.np.edu.sg>
2022-08-03 11:33:23 +0800

    overheads 2

--- project_group/read_file.py
+++ project_group/read_file.py
@@ -4,5 +4,12 @@ import re, csv
 overhead_list = []
 
 def overheads():
-    path = Path.cwd()/'csv_reports'/'Overheads.csv'
-    with path.open
+    path = Path.cwd()/'project_group'/'csv_reports'/'Overheads.csv'
+    with path.open(mode= "r", encoding= "UTF-8") as file:
+        reader= csv.reader(file)
+        next(reader)
+        for line in reader:
+            overhead_list.append([line])
+
+
+print(overhead_list)
