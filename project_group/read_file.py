dd8b4604fb5ed2201dc5d1a9d74bccd360b0eb66
s10220975 <s10220975@connect.np.edu.sg>
2022-08-03 12:42:46 +0800

    sync

--- project_group/read_file.py
+++ project_group/read_file.py
@@ -12,8 +12,22 @@ def overheads():
 
         for line in reader:
             overhead_value.append(float(line[1]))
-            overhead_list.append(line)
             maximum_value = max(overhead_value)
+            overhead_list.append(line)
             return f"{maximum_value}"
     
 print(overheads())
+
+blank_list = []
+
+def cash_on_hand():
+     path = Path.cwd()/'project_group'/'csv_reports'/'cash-on-hand-usd.csv'
+     with path.open(mode = "r",encoding="UTF-8",newline="") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for line in reader:
+            for value in line:
+                blank_list.append(float(value))
+
+            print(blank_list)
