25114a72daa01de07d3f2ac69a6dc9c5eba679f0
Kaylene <s10243062@connect.np.edu.sg>
2022-08-03 23:20:02 +0800

    changes

---
+++ project_group/write_file.py
@@ -0,0 +1,14 @@
+import read_file
+
+list= read_file.overheads()
+
+
+all_overheads= []
+for value in list:
+    all_overheads.append(value[1])
+
+highest_amt= max(all_overheads)
+
+for number, values in enumerate(all_overheads):
+     if values== highest_amt:
+        category= list[number][0]
