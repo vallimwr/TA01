0ad0816f8ce63f684e92b0bce975a30d46aebb15
s10220975 <s10220975@connect.np.edu.sg>
2022-08-04 00:33:40 +0800

    slay to the left and right

--- project_group/write_file.py
+++ project_group/write_file.py
@@ -2,16 +2,18 @@ import read_file
 
 list= read_file.overheads()
 
+def overheads_write():
+    all_overheads= []
+    for value in list:
+        all_overheads.append(value[1])
 
-all_overheads= []
-for value in list:
-    all_overheads.append(value[1])
+    highest_amt= max(all_overheads)
 
-highest_amt= max(all_overheads)
+    for number, values in enumerate(all_overheads):
+        if values== highest_amt:
+            category= list[number][0]
 
-for number, values in enumerate(all_overheads):
-     if values== highest_amt:
-        category= list[number][0]
+    highest_amt = float(highest_amt)
+    return(highest_amt)
 
-highest_amt = float(highest_amt)
-print(highest_amt)
+print(overheads_write())
