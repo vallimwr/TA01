5c55ae0a395750ac6aa206184c279dd1a7763f7e
s10220975 <s10220975@connect.np.edu.sg>
2022-08-03 10:16:30 +0800

    oops

--- project_group/api.py
+++ project_group/api.py
@@ -1,10 +1,9 @@
 import requests
 
-def forex(USD):
-    api_key = "1JZVGRKCHDT3KUSQ"
-    url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
+api_key = "1JZVGRKCHDT3KUSQ"
+url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
 
-    r = requests.get(url)
+r = requests.get(url)
 
-    data = r.json()
-    print(data)
+data = r.json()
+print(data)
