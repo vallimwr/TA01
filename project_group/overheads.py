cdd52e1a36309cd037c672f8571fa3020d75cfd8
Kaylene <s10243062@connect.np.edu.sg>
2022-08-04 10:32:38 +0800

    bye

---
+++ project_group/overheads.py
@@ -0,0 +1,26 @@
+import read_file
+
+list= read_file.overheads()
+
+def overheads_write():
+    all_overheads= []
+    for value in list:
+        all_overheads.append(value[1])
+
+    highest_amt= max(all_overheads)
+    
+
+    for number, values in enumerate(all_overheads):
+        if values== highest_amt:
+            category= list[number][0]
+    return category, highest_amt
+
+
+print(overheads_write())
+
+def cashonhand_write():
+    coh_list = []
+    for value in list:
+        coh_list.append(value[1])
+
+    
