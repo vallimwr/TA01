90a8e8a4ae66eb2aff574286b9f8a12c94f93285
Kaylene <s10243062@connect.np.edu.sg>
2022-08-04 17:26:54 +0800

    sync again

--- project_group/profit_and_loss.py
+++ project_group/profit_and_loss.py
@@ -20,9 +20,33 @@ def profit_and_loss():
 
 def pnl_write(): 
     all_pnl= []
+    day_pnl= []
     for value in profit_and_loss():
         all_pnl.append(float(value[4]))
     print(all_pnl)
+
+    for days in profit_and_loss():
+        day_pnl.append(days[0])
+    print(day_pnl)
+
+    count = 0 
+    for amount in range(len(all_pnl)-1):
+        diff = all_pnl[amount] - all_pnl[amount + 1]
+        if diff >0:
+            with file_path.open(mode = 'w', encoding = 'UTF-8') as file:
+                text = file.write(f"[CASH DEFICIT] DAY: {day_pnl[amount+1]}, AMOUNT: SGD{diff:.2f}")
+                count += 1
+
+        else:
+                print('does not exist')
+
+        if count == 0:
+            with file_path.open(mode = 'w', encoding = 'UTF-8') as file:
+                    text = file.write(f"[PROFIT SURPLUS] Profit on each period is higher than the previous period")
+
+        else:
+            print('does not exist')
+
 
     # for value in enumerate(all_pnl):
     #      if value== deficit_amt:
