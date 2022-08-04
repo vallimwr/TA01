c4865989d0d6f60a1f9f48171fa2972ba10d12b3
s10220975 <s10220975@connect.np.edu.sg>
2022-08-04 11:06:57 +0800

    change

--- project_group/main.py
+++ project_group/main.py
@@ -1,4 +1,11 @@
-import api, overheads, cash_on_hand, profit_and_loss 
+import api, overheads, coh, profit_and_loss 
 from pathlib import Path
 
-file_path = path
+file_path = Path.cwd()/'project group'/'overall_report.txt'
+file_path.touch()
+
+def main_func():
+    forex = api.api_function()
+    overheads.overheads_function(forex)
+    coh.coh_function(forex)
+    profit_and_loss.profitloss_function(forex)
