3e82e17f67539ef72e451775a4645c31b8bc98ff
vallimwr <S10247819@connect.np.edu.sg>
2022-08-04 23:26:43 +0800

    done

--- project_group/main.py
+++ project_group/main.py
@@ -4,6 +4,9 @@ from pathlib import Path
 file_path = Path.cwd()/'project_group'/'overall_report.txt'
 file_path.touch()
 
+'''
+    - To import functions, to coordinate and excute functions
+'''
 def main_func():
     forex = api.api()
     api.api()
