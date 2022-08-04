4ae4f7cd27e3d82ed47f83969501f03e9ccbc41c
Kaylene <s10243062@connect.np.edu.sg>
2022-08-04 15:16:01 +0800

    what.

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -2,19 +2,23 @@
 from pathlib import Path
 import re, csv
 
-def overheads():
-    """
-    empty list made to store overhead values
-    """
-    overheads_list= []
-    path = Path.cwd()/'project_group'/'csv_reports'/'Overheads.csv'
-    with path.open(mode= "r", encoding= "UTF-8") as file:
-        reader= csv.reader(file)
-        next (reader)
+try:
+    def overheads():
+        """
+        empty list made to store overhead values
+        """
+        overheads_list= []
+        path = Path.cwd()/'project_group'/'csv_reports'/'Overheads.csv'
+        with path.open(mode= "r", encoding= "UTF-8") as file:
+            reader= csv.reader(file)
+            next (reader)
 
-        for line in reader:
-            overheads_list.append(line)
-        return overheads_list
+            for line in reader:
+                overheads_list.append(line)
+            return overheads_list
+    print("This is working")
+except Exception as e:
+    print(f"This does not work. Reason: {e}")
         
 #print(overheads())
 
@@ -23,6 +27,7 @@ try:
         all_overheads= []
         for value in overheads():
             all_overheads.append(float(value[1]))
+        print(all_overheads)
 
         highest_amt= max(all_overheads)
         
